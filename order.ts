defe3e51a56908fe3ad7ff0bfbe94a400bbb19f6
arnnvv <arnav.900@protonmail.com>
2024-01-18 04:41:53 +0530

    chkPt2

--- order.ts
+++ order.ts
@@ -2,40 +2,86 @@ import request from "request";
 import crypto from "crypto";
 
 const baseUrl: string = "https://api.coindcx.com";
+import { key, secret } from "./config";
 
 export const createOrder = (
   side: "buy" | "sell",
   market: string,
   price: number,
+  quantity: number,
   clientOrderId: string,
 ) => {
-  const body = {
-    side: "buy", //Toggle between 'buy' or 'sell'.
-    order_type: "limit_order", //Toggle between a 'market_order' or 'limit_order'.
-    market: "SNTBTC", //Replace 'SNTBTC' with your desired market.
-    price_per_unit: "0.03244", //This parameter is only required for a 'limit_order'
-    total_quantity: 400, //Replace this with the quantity you want
-    timestamp: timeStamp,
-    client_order_id: "2022.02.14-btcinr_1", //Replace this with the client order id you want
-  };
+  return new Promise<void>((resolve) => {
+    const body = {
+      side,
+      order_type: "limit_order",
+      market,
+      price_per_unit: price,
+      total_quantity: quantity,
+      timestamp: Math.floor(Date.now()),
+      client_order_id: clientOrderId,
+    };
 
-  const payload = new Buffer(JSON.stringify(body)).toString();
-  const signature = crypto
-    .createHmac("sha256", secret)
-    .update(payload)
-    .digest("hex");
+    const payload = new Buffer(JSON.stringify(body)).toString();
+    const signature = crypto
+      .createHmac("sha256", secret)
+      .update(payload)
+      .digest("hex");
 
-  const options = {
-    url: baseurl + "/exchange/v1/orders/create",
-    headers: {
-      "X-AUTH-APIKEY": key,
-      "X-AUTH-SIGNATURE": signature,
-    },
-    json: true,
-    body: body,
-  };
+    const options = {
+      url: baseurl + "/exchange/v1/orders/create",
+      headers: {
+        "X-AUTH-APIKEY": key,
+        "X-AUTH-SIGNATURE": signature,
+      },
+      json: true,
+      body: body,
+    };
 
-  request.post(options, function (error, response, body) {
-    console.log(body);
+    request.post(options, function (error, response, body) {
+      if (error) {
+        console.log("error while cancelling orders");
+      } else {
+        console.log(body);
+      }
+      resolve();
+    });
+  });
+};
+
+export const cancleOrder = () => {};
+
+export const cancelAll = (market: string) => {
+  return new Promise<void>((resolve) => {
+    const body = {
+      market,
+      timestamp: Math.floor(Date.now()),
+    };
+
+    const payload = new Buffer(JSON.stringify(body)).toString();
+    const signature = crypto
+      .createHmac("sha256", secret)
+      .update(payload)
+      .digest("hex");
+
+    const options = {
+      url: baseurl + "/exchange/v1/orders/cancel_all",
+      headers: {
+        "X-AUTH-APIKEY": key,
+        "X-AUTH-SIGNATURE": signature,
+      },
+      json: true,
+      body: body,
+    };
+
+    request.post(options, function (error, response, body) {
+      if (error) {
+        console.log("error while cancelling orders");
+      } else {
+        console.log("acnceleled all orders");
+        console.log(body);
+      }
+      resolve();
+    });
   });
 };
