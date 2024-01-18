2f83765f2f0286c37eb1d6ab7147a258b2a64ee3
arnnvv <arnav.900@protonmail.com>
2024-01-18 05:33:36 +0530

    almost

--- index.ts
+++ index.ts
@@ -1,24 +1,25 @@
 import { Depthmanager } from "./DepthManagar";
 import { cancelAll, createOrder } from "./order";
 
-const solInrMarket = new Depthmanager("B-SOL_INR");
+const solInrMarket = new Depthmanager("B-XAI_INR");
 const usdtInrMarket = new Depthmanager("B-USDT_INR");
-const solUsdtMarket = new Depthmanager("B-SOL_USDT");
+const solUsdtMarket = new Depthmanager("B-XAI_USDT");
 
 setInterval(() => {
   console.log(solInrMarket.getRelevantDepth());
   console.log(usdtInrMarket.getRelevantDepth());
   console.log(solUsdtMarket.getRelevantDepth());
-
-  const canGetInr = solInrMarket.getRelevantDepth().highestBid - 0.001;
+  const canGetInr = solInrMarket.getRelevantDepth().lowestAsk - 0.001;
   const canGetUsdt = canGetInr / usdtInrMarket.getRelevantDepth().lowestAsk;
   const canGetSol = canGetUsdt / solUsdtMarket.getRelevantDepth().lowestAsk;
-  console.log(`You can now convert $${1} to ${canGetSol} Sol`);
 
-  const initialInr = solInrMarket.getRelevantDepth()?.lowestAsk + 0.001;
-  const canGetUsdt2 = usdtInrMarket.getRelevantDepth()?.highestBid;
-  const canGetInr2 = usdtInrMarket.getRelevantDepth()?.highestBid * canGetUsdt2;
-  console.log(`You can now convert ${initialInr} Sol to ₹${canGetInr2}`);
+  console.log(`You can convert ${1} SOL into ${canGetSol} SOL`);
+
+  const initialInr = solInrMarket.getRelevantDepth().highestBid + 0.001;
+  const canGetUsdt2 = solUsdtMarket.getRelevantDepth().highestBid;
+  const canGetInr2 = canGetUsdt2 * usdtInrMarket.getRelevantDepth().highestBid;
+
+  console.log(`You can convert ${initialInr} INR into ${canGetInr2} INR`);
 }, 2000);
 
 async function main() {
